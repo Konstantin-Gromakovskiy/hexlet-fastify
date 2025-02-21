8761b9eaf88b94887a67f06bab99de221d59dd20
Konstantin <k.gromakovskiy@yandex.ru>
2025-02-21 12:11:15 +0300

    add conditional constructions

---
+++ data.js
@@ -0,0 +1,15 @@
+const state = {
+  courses: [
+    {
+      id: 1,
+      title: 'JS: Массивы',
+      description: 'Курс про массивы в JavaScript',
+    },
+    {
+      id: 2,
+      title: 'JS: Функции',
+      description: 'Курс про функции в JavaScript',
+    },
+  ],
+};
+export default state
--- routes/root.js
+++ routes/root.js
@@ -1,4 +1,17 @@
+import view from '@fastify/view';
+import pug from 'pug';
+import data from '../data.js';
+import path from 'path';
+
+
 export default async function (fastify, opts) {
+  await fastify.register(view, {
+    engine: {
+      pug
+    },
+    root: path.resolve('src/views')
+  })
+  
   fastify.get('/', async function (request, response) {
     return 'Hellow world'
   })
@@ -17,7 +30,16 @@ export default async function (fastify, opts) {
     const userId = request.params.id
     const postId = request.params.postid
     
-    response.cod
     return 'Это пользователь ' + userId + ' и пост ' + postId
   })
+  fastify.get('/courses', (req, res) => res.view('courses', { courses: data.courses }));
+  fastify.get('/courses/:id', (req, res) => {
+    const id = req.params.id
+    const course = data.courses.find((course) => course.id === Number(id))
+    console.log(data)
+    return res.view('course', { course })
+  })
+  
+  
+  
 }
